d2ab56b91b45c05208116428418c7e91063b4d2c
avagoyal <57515691+avagoyal@users.noreply.github.com>
2020-10-09 16:37:53 -0700

    Update header.php

--- header.php
+++ header.php
@@ -8,9 +8,10 @@
 <body>
 	<div class="headerDiv">
 		<h1 class="headerTitle">Z o o m e r</h1>
-		
+
 		<div class = "headerBarDiv">
 			<button class = "signIn" onclick="getInfoFromGoogleClassRoom();">Sign In</button>
+			<button class = "signIn" onclick="getInfoFromGoogleClassRoom();">Get Links</button>
 		</div>
 	</div>
 
